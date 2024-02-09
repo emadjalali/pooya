44b964cbe3e652259ce02386acbdb9959944ed39
emad jalali <emad.jalali@gmail.com>
2024-02-09 10:20:56 +0330

    first try

--- pages/majid.js
+++ pages/majid.js
@@ -1,11 +1,11 @@
-import Page1 from "@/components/page1";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
+// import Page1 from "@/components/page1";
+// import Header from "@/components/header";
+// import Footer from "@/components/footer";
 export default function Home() {
 	return (
 		<div>
 			{/* <Header /> */}
-			<Page1 />
+			{/* <Page1 /> */}
 			{/* <Footer /> */}
 		</div>
 	);
