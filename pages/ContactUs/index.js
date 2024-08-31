200ce5aa1c6f4fbd04f6c6b52c63e4bdc9a46c43
NEF0-0 <sagharkhorshidi5@gmail.com>
2024-08-31 18:52:27 +0330

    add last fr-version

--- pages/ContactUs/index.js
+++ pages/ContactUs/index.js
@@ -20,31 +20,31 @@ export default function ContactUs(){
     return(
         <div>
             <div>
+                <div className="FirstPart">
                 <div className="title">
                     <h1>تماس با ما</h1>
                 </div>
-                <div>
                 <form className="FormsPosition" onSubmit={handleSubmit}>
 					<input
 						id='name'
 						name='name'
 						placeholder='نام'
 						type='name'
-						className='footer-submit-input form'
+						className='form'
 					/><br/>
                     <input
 						id='tel'
 						name='tel'
 						placeholder='شماره همراه'
 						type='tel'
-						className='footer-submit-input form'
+						className='form'
 					/><br/>
                     <input
 						id='email'
 						name='email'
 						placeholder='ایمیل'
 						type='email'
-						className='footer-submit-input form'
+						className='form'
 					/><br/>
                     <select className="select" placeholder='زمینه فعالیت'>
                         <option></option>
@@ -58,31 +58,31 @@ export default function ContactUs(){
 				</form>
                 </div>
                 <div>
-                    <Image src={patternD} className="patternD"></Image>
-                    <div>
+                    <div style={{display:" flex", flexDirection: "column", alignItems: "center"}}><Image src={patternD} className="patternD"></Image></div>
+                    <div className="footer">
                         <div className="NumDivs">
                             <div className="NumEn">
                                 <p style={{paddingLeft:"7px", marginRight:"-20px" , direction:"ltr" , fontSize:"20px"}}>021-22 72 4059</p>
-                                <Image style={{width:"5%"}} src={phone}></Image>
+                                <Image style={{width:"%"}} src={phone}></Image>
                             </div>
                             <div className="NumFr">
                                 <p style={{paddingLeft:"10px" , direction:"ltr" , fontSize:"20px"}}>0936 128 34 61</p>
-                                <Image style={{width:"5%" , marginLeft:"1px"}} src={tel}></Image>
-                                <Image style={{width:"5%"}} src={WU}></Image>
+                                <Image style={{width:"%" , marginLeft:"1px"}} src={tel}></Image>
+                                <Image style={{width:"%"}} src={WU}></Image>
                             </div>
                         </div>
                         <div className="Address">
                             <p>نیاوران،خیابان مژده،<br/>کوچه قادری،پلاک2،<br/>زنگ 5</p>
                         </div>
-                        <div>
+                        <div className="IconDives">
                             <div className="email">
                                 <p style={{fontSize:"17px" , marginLeft:"5px"}}>info@feelingofalook.com</p>
-                                <Image style={{width:"5%"}} src={email}></Image>
+                                <Image style={{width:"%"}} src={email}></Image>
                             </div>
                             <div className="Icons">
-                                <Image style={{width:"6%" , marginLeft:"5px"}} src={X}></Image>
-                                <Image style={{width:"6%" , marginLeft:"5px"}} src={In}></Image>
-                                <Image style={{width:"6%" , marginLeft:"5px"}} src={Insta}></Image>
+                                <Image style={{width:"%" , marginLeft:"5px"}} src={X}></Image>
+                                <Image style={{width:"%" , marginLeft:"5px"}} src={In}></Image>
+                                <Image style={{width:"%" , marginLeft:"5px"}} src={Insta}></Image>
                             </div>
                         </div>
                     </div>
@@ -90,31 +90,43 @@ export default function ContactUs(){
                 </div>
             </div><style>{`
                 .title{
+                    width: 290px;
+                }
+                .FirstPart{
                     display: flex;
-                    width: 55%;
-                    justify-content: center;
+                    flex-direction: column;
+                    align-items: center;
+                    width: 100%;
                 }
                 .form{
-                    padding-left: 160px;
+                    width: 160%;
                     height: 30px;
                     margin-bottom: -20px;
                     text-align: end;
                     direction: ltr;
+                    padding: 5px;
+                    border: none;
+                    background-color: rgb(238, 238, 238);
                 }
                 .select{
-                    max-width: 70%;
-                    background-color: #eeeeee;
+                    width: 160%;
                     height: 30px;
-                    margin-top: 25px;
-                    padding-left: 289px;
+                    margin-top: 30px;
+                    text-align: end;
+                    direction: ltr;
+                    padding: 5px;
+                    border: none;
+                    background-color: rgb(238, 238, 238);
                 }
                 .textArea{
-                    max-width: 70%;
-                    background-color: #eeeeee;
-                    margin-top: 25px;
-                    height: 100px;
-                    padding-left: 175px;
-                    padding-right:5px
+                    width: 160%;
+                    height: 120px;
+                    margin-top: 30px;
+                    text-align: end;
+                    direction: ltr;
+                    padding: 5px;
+                    border: none;
+                    background-color: rgb(238, 238, 238);
                 }
                 .button{
                     padding: 8px 10px 8px 10px;
@@ -125,9 +137,9 @@ export default function ContactUs(){
                     font-size: 12px;
                 }
                 .FormsPosition{
-                        display: flex;
-                        flex-direction: column;
-                        align-items: center;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
                 }
                 .patternD{
                     display:none
@@ -170,13 +182,44 @@ export default function ContactUs(){
                     display: flex;
                     justify-content: center;
                     margin-top: -10px;
-
+                    margin-bottom: 0px;
                 }
                 .patternM{
                     margin: 50px 0px 80px 0px;
                     display: flex;
                     flex-direction: column;
                     align-items: center;
+                }
+                @media screen and (min-width: 768px) {
+                   .FirstPart{
+                      width:55%
+                   }
+                    .patternD{
+                        display: block;
+                        margin: 50px 0px 80px 0px;
+                        width: 980px;
+
+                    }
+                    .patternM{
+                        display:none
+                    }
+                    .footer{
+                        display: flex;
+                        flex-direction: row-reverse;
+                        align-items: flex-end;
+                        justify-content: space-around;
+                        margin-bottom: 50px;
+                    }
+                    .NumDivs{
+                        align-items: flex-end;
+                    }
+                    .IconDives{
+                        display: flex;
+                        flex-direction: column-reverse;
+                        align-items: center;
+                    }
+                    .Icons{margin-bottom: -45px;}
+                    .Address{width: 245px;}
                 }
             `}</style>  
         </div>
